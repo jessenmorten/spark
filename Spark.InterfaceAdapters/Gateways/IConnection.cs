6aa1cebf54efb016f61f3c111d9cfe8b2922185b
Morten Jessen <mortenjessen5d@gmail.com>
2023-11-21 19:53:14 +0100

    refactor: remove unused methods

--- Spark.InterfaceAdapters/Gateways/IConnection.cs
+++ Spark.InterfaceAdapters/Gateways/IConnection.cs
@@ -6,8 +6,6 @@ namespace Spark.InterfaceAdapters.Gateways;
 public interface IConnection<TEntityData> where TEntityData : IEntityData
 {
     string Id { get; }
-    Task<int> ReceiveAsync(ArraySegment<byte> data);
-    Task<int> SendAsync(ArraySegment<byte> data);
     Task UpdateAsync(TEntityData entityData);
 }
 
