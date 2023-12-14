3d5018b1bd016a197727fcc13dd8a94ddd1494b2
Morten Jessen <mortenjessen5d@gmail.com>
2023-12-14 22:46:07 +0100

    feat: close unhealthy connections

--- Spark.Hub/ConnectionManager.cs
+++ Spark.Hub/ConnectionManager.cs
@@ -11,6 +11,17 @@ public class ConnectionManager<TDeviceData> : IConnectionManager<TDeviceData> wh
 
     public int Count => _connections.Count;
 
+    public ConnectionManager()
+    {
+        var cancellationToken = CancellationToken.None; // TODO
+
+        Task.Factory.StartNew(
+            () => CloseUnhealthyConnectionsLoop(cancellationToken),
+            cancellationToken,
+            TaskCreationOptions.LongRunning,
+            TaskScheduler.Default);
+    }
+
     public void Add(IConnection<TDeviceData> connection)
     {
         _ = connection ?? throw new ArgumentNullException(nameof(connection));
@@ -28,5 +39,24 @@ public class ConnectionManager<TDeviceData> : IConnectionManager<TDeviceData> wh
         var success = _connections.TryGetValue(deviceId, out var conn);
         connection = conn;
         return success;
+    }
+
+    private async Task CloseUnhealthyConnectionsLoop(CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(30), cancellationToken);
+
+            var connections = _connections.Values.ToArray();
+            foreach (var connection in connections)
+            {
+                if (!connection.Healthy)
+                {
+                    connection.Close();
+                    _connections.TryRemove(connection.DeviceId, out _);
+                }
+            }
+
+        }
     }
 }
--- Spark.UseCases/IConnection.cs
+++ Spark.UseCases/IConnection.cs
@@ -4,6 +4,7 @@ namespace Spark.UseCases;
 
 public interface IConnection<TDeviceData> where TDeviceData : IDeviceData
 {
+    bool Healthy { get; }
     string DeviceId { get; }
     Task UpdateAsync(TDeviceData deviceData, CancellationToken cancellationToken);
     void Close();
